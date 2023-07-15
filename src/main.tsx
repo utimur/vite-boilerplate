a3a809a5ca2257a10fd2f7d092312ecde4d19dfd
utimur <akva_ten@mail.ru>
2023-07-15 16:09:16 +0300

    add query params

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,9 @@ import {BrowserRouter} from "react-router-dom";
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+    <React.StrictMode>
+            <BrowserRouter>
+                    <App/>
+            </BrowserRouter>
+    </React.StrictMode>
 )
